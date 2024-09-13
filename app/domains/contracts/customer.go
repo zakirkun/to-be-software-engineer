4095cfddb54a34c922bc3a8b3b762237c8f00b9a
Muhammad Iqbal <julkifli.muhammad@gmail.com>
2024-09-13 22:48:47 +0700

    fix: remove comment code

--- app/domains/contracts/customer.go
+++ app/domains/contracts/customer.go
@@ -9,20 +9,14 @@ import (
 type CustomerRepository interface {
 	Insert(data models.Customer) (*models.Customer, error)
 	GetByUsername(username string) (*models.Customer, error)
-	//GetAll() (*[]models.Category, error)
-	//Show(categoryId int) (*models.Category, error)
 }
 
 type CustomerServices interface {
 	AddCustomer(request types.RequestCreateCustomer) (*types.ResponseCreateCustomer, error)
 	Login(customer types.RequestLogin) (*types.ResponseLogin, error)
-	//GetAll() (*types.ResponseListCategory, error)
-	//Show(categoryId int) (*types.ResponseCreateCategory, error)
 }
 
 type CustomerController interface {
 	Register(ctx echo.Context) error
 	Login(ctx echo.Context) error
-	//GetAll(ctx echo.Context) error
-	//Show(ctx echo.Context) error
 }
